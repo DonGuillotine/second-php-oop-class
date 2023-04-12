83afd09fe482e749b23d470737cfedb157ab0b26
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-12 11:27:31 +0100

    More on Objects

--- oop_intro.php
+++ oop_intro.php
@@ -18,16 +18,36 @@
         function retrieve_your_name(){
             return $this->name;
         }
+
+        function set_color($color){
+            $this->color = $color;
+        }
+
+        function get_color(){
+            return $this->color;
+        }
     }
 
     // Created a new object / instantiated the class
     $apple = new Fruit();
     $banana = new Fruit();
 
+    $green = new Fruit();
+    $yellow = new Fruit();
+
     $apple->insert_your_name('Applepie');
     $banana->insert_your_name('Bananapie');
+
+    $green->set_color("Green");
+    $yellow->set_color("Yellow");
 
     echo $apple->retrieve_your_name();
     echo "<br>";
     echo $banana->retrieve_your_name();
+
+    echo "<br>";
+    
+    echo $green->get_color();
+    echo "<br>";
+    echo $yellow->get_color();
 ?>
