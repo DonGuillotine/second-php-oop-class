2499e83af2363ada1987103443d65c51f6391fc5
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-14 10:39:49 +0100

    PHP Access Modifier I

--- access_modifiers.php
+++ access_modifiers.php
@@ -3,10 +3,26 @@
         public $name;
         protected $color;
         private $height;
+
+        function set_name($age){
+            $this->name = $age;
+        }
+
+        protected function set_color($age){
+            $this->color = $age;
+        }
+
+        private function set_height($age){
+            this->height = $age;
+        }
     }
 
     $flutter = new Material();
-    $flutter->name = 'Mango';
-    $flutter->color = 'Blue';
-    $flutter->height;
+    // $flutter->name = 'Mango';
+    // $flutter->color = 'Blue';
+    // $flutter->height;
+
+    $flutter->set_name('50');
+    $flutter->set_color('000');
+    $flutter->set_height('122');
 ?>
