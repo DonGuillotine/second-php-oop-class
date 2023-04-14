9bf81386a4a74d785cff0f7d94db2c75a7ccffac
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-14 10:20:42 +0100

    PHP Destructors

---
+++ descructors.php
@@ -0,0 +1,16 @@
+<?php 
+    class JavaScript{
+        public $name;
+        public $color;
+
+        function __construct($name)
+        {
+            $this->name = $name;
+        }
+
+        function __destruct()
+        {
+            echo "JavaScript is $this->name";
+        }
+    }
+?>
