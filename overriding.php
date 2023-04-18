ee7eff65e9fd423664b2ee6d96a11f43b53b0089
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-18 10:56:49 +0100

    PHP Overriding Methods I

---
+++ overriding.php
@@ -0,0 +1,36 @@
+<?php 
+    /**
+     * Inherited methods can be overridden by redefining the methods with the same name in the child class
+     */
+
+     class Students{
+        public $name;
+        public $laptop;
+
+        public function __construct($name, $laptop)
+        {
+            $this->name = $name;
+            $this->laptop = $laptop;
+        }
+        public function message(){
+            echo "The Student is $this->name and the laptop is $this->laptop";
+        }
+     }
+
+     class PhpStudents extends Students{
+        public $xampp;
+        public function __construct($name, $laptop, $xampp)
+        {
+            $this->name = $name;
+            $this->laptop = $laptop;
+            $this->xampp = $xampp;
+        }
+        public function message()
+        {
+            echo "The Student is $this->name, the laptop is $this->laptop and the server is $this->xampp";
+        }
+     }
+
+     $developers = new PhpStudents("Ebuka", "Hewlett Peckett", "XAMPP");
+     $developers->message();
+?>
