348fa0ebfd89668fc4c3a4aaba2ceab380753e74
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-28 12:59:46 +0100

    PHP Static Methods II

--- static_methods.php
+++ static_methods.php
@@ -8,4 +8,18 @@
 
     // Class name followed by the Scope Resolution Operator
     Chelsea::winners();
+
+    // More on Static Methods
+    class Greeting{
+        public static function welcome(){
+            echo "Hello World";
+        }
+
+        public function __construct()
+        {
+            self::welcome();
+        }
+    }
+    
+    new Greeting();
 ?>
